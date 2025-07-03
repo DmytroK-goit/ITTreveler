4b99d0973ef3b6afd9dc7d4ca84cdc164109202e
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-07-03 21:36:35 +0300

    add main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#F3743D',
-        gray: '#939393',
-        black: '#2C2C2C',
+        primary: '#f3743d',
       },
     },
   },
