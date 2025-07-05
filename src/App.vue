19e68fd75e29d571397ff43f537515d6e22a9665
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-07-05 21:04:09 +0300

    work with v-for

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup>
 import HomepageView from './views/HomepageView.vue'
 
-const isDesktop = window.innerWidth > 1024
+const fruits = ['kiwi', 'banana', 'apple', 'orange', 'grape']
+const fruitsMap = {
+  kiwi: 'Super kiwi',
+  banana: 'Super banana',
+  apple: 'Super apple',
+  orange: 'Super orange',
+  grape: 'Super grape',
+}
+
+// const isDesktop = window.innerWidth > 1024
 // const handleResize = () => {
 //   isDesktop.value = window.innerWidth > 1024
 // }
-const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
+// const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
 </script>
 <template>
-  <template v-if="isDesktop">
-    <HomepageView />
-  </template>
-  <template v-else-if="isTablet">This is tablet</template>
-  <template v-else>
-    <div v-show="!isDesktop && !isTablet">This is mobile</div>
-  </template>
-  <div v-show="!isDesktop && !isTablet">This is mobile</div>
+  <div :key="fruit" v-for="(fruit, index) in fruits">{{ index }}- {{ fruit }}</div>
+  <div :key="fruit" v-for="(fruit, key) in fruitsMap">{{ key }}-{{ fruit }}</div>
+  <div :key="ch" v-for="ch in 'Pruvit'">{{ ch }}</div>
+
+  <HomepageView />
 </template>
