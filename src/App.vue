bbdabd8eaa3432f77e585eb45d8030e12115d304
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-07-07 21:49:17 +0300

    add ref

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <script setup>
-import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
 import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
-import HomepageView from './views/HomepageView.vue'
-
-// const isDesktop = window.innerWidth > 1024
-// const handleResize = () => {
-//   isDesktop.value = window.innerWidth > 1024
-// }
-// const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
 </script>
 <template>
-  <!-- <div :key="fruit" v-for="(fruit, index) in fruits">{{ index }}- {{ fruit }}</div>
-  <div :key="fruit" v-for="(fruit, key) in fruitsMap">{{ key }}-{{ fruit }}</div>
-  <div :key="ch" v-for="ch in 'Pruvit'">{{ ch }}</div> -->
-
-  <!-- <HomepageView /> -->
   <FavoritePlaces />
 </template>
