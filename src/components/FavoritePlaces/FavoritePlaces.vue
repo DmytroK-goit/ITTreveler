aed0d59edf10bd150bddb3a722cc523fec43b890
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-07-09 21:55:46 +0300

    add events

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+import IInput from '../IInput/IInput.vue'
 const btnVariant = ref('gradient')
 
 const changeBtnVar = () => {
@@ -12,6 +13,10 @@ const changeBtnVar = () => {
 <template>
   <div class="px-6">
     <div class="text-grey mb-4">Додані маркери {{ counter }}</div>
+    <div class="py-5">
+      <IInput label="Some label" />
+      <a href="/" class="text-bolt" @click.prevent="() => console.log('prevent')">click</a>
+    </div>
     <FavoritePlace v-for="n in 4" :key="n" />
     <IButton class="w-full mt-10" :variant="btnVariant" @click="changeBtnVar"
       >Додати маркер</IButton
---
+++ src/components/IInput/IInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  label: String,
+})
+
+const text = ref('')
+const changeValue = (e) => {
+  text.value = e.target.value
+}
+</script>
+
+<template>
+  <div class="w-full text-[#2c2c2c]">
+    <label for="" class="block">
+      <span class="block text-xs px-3 mb-2">{{ props.label }}:{{ text }}</span>
+      <input
+        class="w-full text-sm rounded-[4px] border-[1px] py-2 px-3 focus:outline-primary"
+        type="text"
+        @input="changeValue"
+      />
+    </label>
+  </div>
+</template>
