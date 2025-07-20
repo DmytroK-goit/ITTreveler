4036cf50860bbb5cc150089b756d0a904cd87cb7
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-07-20 22:19:58 +0300

    add points and axios

---
+++ src/api/user/index.js
@@ -0,0 +1,16 @@
+import { clientFetch } from '../clientFetch'
+
+export const login = (body) => {
+  return clientFetch.post('/users/login', body)
+}
+
+export const registerUser = (body) => {
+  return clientFetch.post('/users/register', body)
+}
+
+export const logout = () => {
+  return clientFetch.get('/users/logout')
+}
+export const refresh = () => {
+  return clientFetch.get('/users/refresh')
+}
